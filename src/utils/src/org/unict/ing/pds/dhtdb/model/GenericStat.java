77ee59c2273cb72608d1499b68a641ee942d3d32
Marco Grassia <marco.grassia@studium.unict.it>
2018-01-29 13:02:12 +0100

    Add Utils app + common Data Model
    Add Data model:
    - GenericStat
    - IOStat
    - NetworkStat
    - RAMStat
    - UptimeStat
    
    Refers #17

---
+++ src/utils/src/org/unict/ing/pds/dhtdb/model/GenericStat.java
@@ -0,0 +1,29 @@
+package org.unict.ing.pds.dhtdb.model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/***
+ * 
+ * @author Marco Grassia <marco.grassia@studium.unict.it>
+ */
+public abstract class GenericStat {
+    private final long timestamp;
+    private String scannerId;
+
+    public GenericStat(long timestamp, String scannerId) {
+        this.timestamp = timestamp;
+        this.scannerId = scannerId;
+    }
+
+    public String getScannerId() {
+        return scannerId;
+    }
+    
+    public long getTimestamp() {
+        return timestamp;
+    }    
+}
---
+++ src/utils/src/org/unict/ing/pds/dhtdb/model/IOStat.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unict.ing.pds.dhtdb.model;
+
+
+public class IOStat extends GenericStat {
+    private final String disk;
+    private final float readKBps, writeKBps;
+
+    public IOStat(String disk, float readKBps, float writeKBps, long timestamp, String scannerId) {
+        super(timestamp, scannerId);
+        this.disk = disk;
+        this.readKBps = readKBps;
+        this.writeKBps = writeKBps;
+    }
+
+    public String getDisk() {
+        return disk;
+    }
+
+    public float getReadKBps() {
+        return readKBps;
+    }
+
+    public float getWriteKBps() {
+        return writeKBps;
+    }
+}
