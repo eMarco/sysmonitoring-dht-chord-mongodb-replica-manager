e068ae8384d89688d296c2eaaac9213099ee8b40
aleskandro <aleskandro@lucylaika.ovh>
2018-01-31 17:11:29 +0100

    Updated run.sh for jboss containers

--- docker/replicamanager/run.sh
+++ docker/replicamanager/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mongod &
-sudo -u jboss ln -s /code/target/*.ear /opt/jboss/wildfly/standalone/deployments/$(basename $(ls /code/target/*.ear))
-
+for i in $(ls /code/target/*.ear); do
+    ln -s $i /opt/jboss/wildfly/standalone/deployments/$(basename $i)
+done
 sudo -u jboss /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 
