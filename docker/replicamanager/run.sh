4ff9af3bfb37b739a393059cacaea72aa6bc4668
aleskandro <aleskandro@lucylaika.ovh>
2018-01-31 11:08:21 +0100

    Fix for run.sh and symbolic linking creation

--- docker/replicamanager/run.sh
+++ docker/replicamanager/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mongod &
-ln -s /code/*.ear /opt/jboss/wildfly/standalone/deployments/$(ls /code/*.ear)
+ln -s /code/*.ear /opt/jboss/wildfly/standalone/deployments/$(basename $(ls /code/*.ear))
 
 sudo -u jboss /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 
