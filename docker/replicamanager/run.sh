2641df4ad17d75385e66ea98ece81f268bd20e2f
aleskandro <aleskandro@lucylaika.ovh>
2018-01-31 12:05:13 +0100

    Fix to deal with target/ elimination on clean & build

--- docker/replicamanager/run.sh
+++ docker/replicamanager/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mongod &
-ln -s /code/*.ear /opt/jboss/wildfly/standalone/deployments/$(basename $(ls /code/*.ear))
+sudo -u jboss ln -s /code/target/*.ear /opt/jboss/wildfly/standalone/deployments/$(basename $(ls /code/target/*.ear))
 
 sudo -u jboss /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 
