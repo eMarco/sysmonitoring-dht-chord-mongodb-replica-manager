0fd5aaad06255aeda98d7124e67dadfbc7ce1bba
aleskandro <aleskandro@lucylaika.ovh>
2018-01-31 11:00:47 +0100

    Fixing for JBoss deployment directory

--- docker/replicamanager/run.sh
+++ docker/replicamanager/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mongod &
+ln -s /code/*.ear /opt/jboss/wildfly/standalone/deployments/$(ls /code/*.ear)
+
 sudo -u jboss /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 
