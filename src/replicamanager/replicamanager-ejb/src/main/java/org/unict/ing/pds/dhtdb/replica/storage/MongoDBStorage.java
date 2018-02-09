97879f55d9702b0d6f29c682fbf2279c1fc51fe3
aleskandro <aleskandro@lucylaika.ovh>
2018-02-09 20:03:29 +0100

    Updated java files

--- src/replicamanager/replicamanager-ejb/src/main/java/org/unict/ing/pds/dhtdb/replica/storage/MongoDBStorage.java
+++ src/replicamanager/replicamanager-ejb/src/main/java/org/unict/ing/pds/dhtdb/replica/storage/MongoDBStorage.java
@@ -1,7 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2018 aleskandro - eMarco
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.unict.ing.pds.dhtdb.replica.storage;
 
@@ -28,7 +39,7 @@ public class MongoDBStorage implements Storage {
         // Using a single connection to provide better (query-oriented) scalability
         this.db = db;
         Jongo jongo = new Jongo(db);
-        this.collection = jongo.getCollection("lightMonitor45");
+        this.collection = jongo.getCollection("lightMonitordf5758a");
     }
     /**
      * Insert
