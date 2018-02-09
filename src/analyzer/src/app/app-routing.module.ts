4b745f205910448e3557730d0b191d4cbb5f8622
Marco Grassia <marco.grassia@studium.unict.it>
2018-02-09 18:54:36 +0100

    Analyzer: remove Home component

--- src/analyzer/src/app/app-routing.module.ts
+++ src/analyzer/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent }        from './home/home.component';
 import { AnalyzerComponent }    from './analyzer/analyzer.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'analyzer', pathMatch: 'full' },
   { path: 'analyzer', component: AnalyzerComponent },
 ];
 
--- src/analyzer/src/app/app.module.ts
+++ src/analyzer/src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
-import { HttpModule }                            from '@angular/http';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { HttpClientModule }     from '@angular/common/http';
+import { HttpModule }           from '@angular/http';
 
 import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent }         from './app.component';
 
 import { NavmenuComponent }     from './navmenu/navmenu.component';
-import { HomeComponent }        from './home/home.component';
 import { AnalyzerComponent }    from './analyzer/analyzer.component';
 
 @NgModule({
@@ -23,10 +22,8 @@ import { AnalyzerComponent }    from './analyzer/analyzer.component';
   declarations: [
     AppComponent,
     NavmenuComponent,
-    HomeComponent,
     AnalyzerComponent
   ],
-  // providers: [ HeroService, MessageService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
